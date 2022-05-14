2a3c132a2ae4c90be85ee0768f7a44f4d11e3cec
larissa.rocha@fpf.br <larissa.rocha@fpf.br>
2022-05-14 10:16:36 -0300

    splash login screen

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color mBackgroundColor = const Color(0xFFF3E5D0);
+
+Color mBackgroundOptionColor = Color(0xFFDAAA63);
+
+Color mPrimaryColor = const Color(0xFF2C1810);
+
+Color mPrimaryTextColor = Color(0xFF848484);
+
+Color mSecundaryColor = Color(0xFFD45400);
